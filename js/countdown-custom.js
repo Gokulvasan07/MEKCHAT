683795800d2bd86e2553acb79fa73c7910004704
Gokulvasan07 <131930235+Gokulvasan07@users.noreply.github.com>
2023-08-08 11:12:44 +0530

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 2, 23)}); // year, month, date, hour
+        });
+});		
+
